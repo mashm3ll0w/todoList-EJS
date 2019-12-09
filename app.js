8328f7a7e387a5a839493802a52a413034086df7
mashm3ll0w <charlswall@live.com>
2019-12-09 19:00:20 +0300

    test the template with days of the week

--- app.js
+++ app.js
@@ -12,6 +12,33 @@ app.get("/", (req, res) => {
 	var currentDay = today.getDay();
 	var day = "";
 
+	switch (currentDay) {
+		case 0:
+			day = "Sunday";
+			break;
+		case 1:
+			day = "Monday";
+			break;
+		case 2:
+			day = "Tuesday";
+			break;
+		case 3:
+			day = "Wednesday";
+			break;
+		case 4:
+			day = "Thursday";
+			break;
+		case 5:
+			day = "Friday";
+			break;
+		case 6:
+			day = "Saturday";
+			break;
+		default:
+			console.log("Error!! The day today is " + currentDay);
+			break;
+	}
+
 	res.render("list", { dayOfWeek: day });
 });
 
